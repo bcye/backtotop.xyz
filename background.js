596c67dc217b22e36cbf0b6262ce9fe37e61c848
bcye <br.apps@icloud.com>
2019-10-15 19:44:45 +0200

    add links

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
-/* chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason === "install") {
-  // TODO: Open Hello Page
-    }
-}); */
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason === "install") {
+    chrome.tabs.create({
+      url:
+        "https://www.notion.so/Thank-you-for-installing-backtotop-xyz-3bc2d450aa80486589779f09a9c14d53"
+    });
+  }
+});
 
-// TODO: chrome.runtime.setUninstallURL("typeform link");
+TODO: chrome.runtime.setUninstallURL("https://bruce160.typeform.com/to/Tf3uCq");
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   console.log("test");
   chrome.tabs.executeScript(tab.id, {
     code: "window.scrollTo(0, 0);"
   });
-  //   chrome.runtime.sendMessage("pageActionClicked");
 });
